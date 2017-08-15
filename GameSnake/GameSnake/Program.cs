4b581a318fa31c6d69ec57a50b6adc4b64043f4f
gggxxxggg <stepanpop235@gmail.com>
2017-08-15 13:32:22 +0300

    замена типа даных String типом char

--- GameSnake/GameSnake/Program.cs
+++ GameSnake/GameSnake/Program.cs
@@ -12,20 +12,20 @@ namespace GameSnake
         {
             int x1 = 1;
             int y1 = 3;
-            String sym1 = "*";
+            char sym1 = '*';
 
             Drow(x1,y1, sym1);
 
             int x2 = 5;
             int y2 = 5;
-            String sym2 = "#";
+            char sym2 = '#';
 
             Drow(x2, y2, sym2);
 
             Console.ReadKey();
 
         }
-        static void Drow (int x, int y, String sym) 
+        static void Drow (int x, int y, char sym) 
         {
             Console.SetCursorPosition(x, y);
             Console.Write(sym);
